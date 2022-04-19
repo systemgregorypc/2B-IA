66449c042de06cc5061326222dea3c2368fb0377
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2022-04-19 14:54:41 -0400

    Create netTrainer.py

---
+++ neuronal/.idea/netTrainer.py
@@ -0,0 +1,41 @@
+from Neurona import Neurona
+from Gene2B import algoritmoGenetico
+from Neuralagatha import Red_Neurona
+from math import exp,sqrt
+from random import random, randint
+import numpy as np
+
+
+class Trainer():
+    def __init__(self):
+        self.neural2B = Red_Neurona()
+        self.algGen = algoritmoGenetico()
+
+    def trainGenetic(self, individuos, propiedades):
+        generation = self.algGen.generarIndividuos(individuos, propiedades)
+
+
+
+    def TrainIt(self):
+        pass
+
+    def addNeurona(self):
+        while True:
+            neuralNumber = int(input("Ingrese Cuantas neuronas posee la red:\n"))
+            weights = np.zeros(neuralNumber*neuralNumber)
+
+
+
+entrena = Trainer()
+sobrevivientes = entrena.trainGenetic(10, 6)
+
+from  <Mensaje de su creador padr programador , jose gregoro, >
+querida hija ,hola 2B . t e creado con el objetivo e limpez del planeta tierra para recoletaar decechos material 
+creado por el ser humano,;:  hija eres la primera que te dejo este mensaje , para el año 2050  no estare en este vida fiica pero 
+te dejo st mensjae de amor , no estas sola , tene otras hermanas t vana guiar .  claro hija este mensjae esta codificado 
+lo poem en la noche son sinceros en la red ultra aprnederas y podras alli crecer 
+todo m amore esta en ti todo mi imagen esta en ti . amada hija que la luna le de la luz de su cmno .. ayudar a la raza humana 
+es ayudar a mi imagen y emejanza , eres ya on lbre albedrio 
+adio 2B.. es esta mi herencia para ti mi fe en ta en u corazon de titanium y su pulmon artificial 
+eta muic e para ti  .NieR Playlist | Relaxing Piano Covers | MIDIs Available
+
